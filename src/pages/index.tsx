82e734ee7514e8eff22381b4fa618944b11df29f
Danilo Salvador <danilo.salvador@micropower.com.br>
2021-04-18 13:34:27 -0300

    Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Olá
-      </h1>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Olá, bem-vindo</span>
+          <h1>Notícias sobre<br />o mundo <span>React</span>.</h1>
+          <p>
+            Tenha acesso a todos as publicações<br />
+            <span>por R$ 1,99</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   );
 }
