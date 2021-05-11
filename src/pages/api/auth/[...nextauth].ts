768cef5296000a366b19b58b67eadea84a102e89
Danilo Salvador <danilo.salvador@micropower.com.br>
2021-05-10 23:45:07 -0300

    Salvando usuários no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
+import { query as q} from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
+
 // https://next-auth.js.org/getting-started/example
 export default NextAuth({
   providers: [
@@ -10,5 +14,27 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
-  database: process.env.DATABASE_URL,
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        // Fauna: https://dashboard.fauna.com
+        // Configuração:
+        // - Criar uma collection: users
+        // - Criar um index com Terms: data.email
+
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  },
 });
