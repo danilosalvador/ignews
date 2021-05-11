7345bdea853a22bf1a889692985e8bb5807ab413
Danilo Salvador <danilo.salvador@micropower.com.br>
2021-05-10 22:10:35 -0300

    Parametrização nas rotas

--- src/pages/api/users.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default (request: NextApiResponse, response: NextApiResponse) => {
-  const users =[
-    { id: 1, name: 'Danilo' },
-  ];
-
-  return response.json(users);
-}
